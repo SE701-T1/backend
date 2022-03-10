f5d3d8802d41bedaf5a4bf68e01f7404bddb8321
ff-dev-45 <flynn.fromont@gmail.com>
2022-03-10 19:57:02 +1300

    Setup of the backend repo
    - Added .gitignore to help prevent unwanted files being added
    - Created Maven project and added dependencies for Junit 5 and Spring within
    - Used intellj project creation to autogenerate folder structure

---
+++ src/test/java/com/team701/buddymatcher/BuddyMatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team701.buddymatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuddyMatcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
