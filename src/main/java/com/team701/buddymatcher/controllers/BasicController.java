cdb367129cd761864510d6e685a61f6559f4da44
ffro805 <66896513+ff-dev-45@users.noreply.github.com>
2022-03-11 00:39:03 +1300

    Basic endpoint (#14)
    * Created basic controller/service to test server
    
    + Needed to add h2 database as a possible temporary measure, since Spring required a database source before starting the server
    
    * Added some comments for the different classes

---
+++ src/main/java/com/team701/buddymatcher/controllers/BasicController.java
@@ -0,0 +1,32 @@
+package com.team701.buddymatcher.controllers;
+
+import com.team701.buddymatcher.services.BasicService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * A Basic controller for handling of user requests, this is only for testing to ensure the server is running properly
+ */
+@RestController
+@RequestMapping("/api/basic")
+public class BasicController {
+
+    private final BasicService basicService;
+
+    @Autowired
+    public BasicController(BasicService basicService) {
+        this.basicService = basicService;
+    }
+
+    @GetMapping(path = "/hello/{name}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> hello(@PathVariable("name") String name) {
+        String response = basicService.helloWorld(name);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
