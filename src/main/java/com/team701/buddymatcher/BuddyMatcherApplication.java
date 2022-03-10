a3d63ea8b0a279f23f2bc79c04f97344335b7746
ffro805 <66896513+ff-dev-45@users.noreply.github.com>
2022-03-10 20:21:13 +1300

    Setup of the backend repo (#10)
    * Setup of the backend repo
    
    - Added .gitignore to help prevent unwanted files being added
    - Created Maven project and added dependencies for Junit 5 and Spring within
    - Used intellj project creation to autogenerate folder structure
    
    * Update .gitignore to keep consistent with frontend
    
    Co-authored-by: ZHENK <chengzhenyang@gmail.com>

---
+++ src/main/java/com/team701/buddymatcher/BuddyMatcherApplication.java
@@ -0,0 +1,13 @@
+package com.team701.buddymatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuddyMatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BuddyMatcherApplication.class, args);
+    }
+
+}
