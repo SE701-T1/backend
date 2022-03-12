47018ab30be7b723d0922db1db31aee9578659f2
Eric Stuart <ejstuart2107@gmail.com>
2022-03-12 20:45:39 +1300

    Added Swagger (#26)
    * Added swagger 2.0 functionality

--- src/main/java/com/team701/buddymatcher/BuddyMatcherApplication.java
+++ src/main/java/com/team701/buddymatcher/BuddyMatcherApplication.java
@@ -2,12 +2,17 @@ package com.team701.buddymatcher;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 
 @SpringBootApplication
+@EnableWebMvc
 public class BuddyMatcherApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BuddyMatcherApplication.class, args);
     }
 
+
 }
+
