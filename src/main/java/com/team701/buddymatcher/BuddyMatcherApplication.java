5f895275807e083d5ed112dc5933085d8d9bf51d
ejstuart <estu836@aucklanduni.ac.nz>
2022-03-12 20:10:16 +1300

    cleaning up imports

--- src/main/java/com/team701/buddymatcher/BuddyMatcherApplication.java
+++ src/main/java/com/team701/buddymatcher/BuddyMatcherApplication.java
@@ -3,12 +3,6 @@ package com.team701.buddymatcher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import org.springframework.context.annotation.Bean;
-
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 
 @SpringBootApplication
