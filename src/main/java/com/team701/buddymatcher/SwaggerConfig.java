c843f48b9ea20c858ddb71c41ff580f20a9c7521
ejstuart <estu836@aucklanduni.ac.nz>
2022-03-12 20:08:16 +1300

    Added swagger 2.0 functionality

---
+++ src/main/java/com/team701/buddymatcher/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.team701.buddymatcher;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+import java.util.Collections;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@Configuration
+public class SwaggerConfig {
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo("MyApp Rest APIs",
+                "APIs for MyApp.",
+                "1.0",
+                "Terms of service",
+                new Contact("test", "www.org.com", "test@emaildomain.com"),
+                "License of API",
+                "API license URL",
+                Collections.emptyList());
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
