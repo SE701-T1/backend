d7648b36333f3175a7da3259f59f9ffa86dced28
dogeliness <dogeofverymanymuchwow@gmail.com>
2022-03-14 21:47:41 +1300

    Data classes

---
+++ src/main/java/com/team701/buddymatcher/domain/communication/Message.java
@@ -0,0 +1,57 @@
+package com.team701.buddymatcher.domain.communication;
+
+import com.team701.buddymatcher.domain.users.User;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name = "<MESSAGES")
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @ManyToOne
+    private User sender;
+    @ManyToOne
+    private User receiver;
+    private long timestamp;
+    private String content;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
