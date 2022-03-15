73af86b7351684b25672c2b182fafe2379e23157
dogeliness <dogeofverymanymuchwow@gmail.com>
2022-03-15 17:41:06 +1300

    Message data class

--- src/main/java/com/team701/buddymatcher/dtos/communication/MessageDTO.java
+++ src/main/java/com/team701/buddymatcher/dtos/communication/MessageDTO.java
@@ -1,17 +1,15 @@
 package com.team701.buddymatcher.dtos.communication;
 
-import java.util.Date;
-
 /**
  * A DTO with info about a message send from user to user
  */
 public class MessageDTO {
     private String sender;
     private String receiver;
-    private Date timestamp;
+    private long timestamp;
     private String content;
 
-    public MessageDTO(String sender, String receiver, Date timestamp, String content) {
+    public MessageDTO(String sender, String receiver, long timestamp, String content) {
         this.sender = sender;
         this.receiver = receiver;
         this.timestamp = timestamp;
