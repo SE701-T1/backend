9c223a5be00ca0c8769ce27ee7ebd8e255cec1c5
dogeliness <57549771+dogeliness@users.noreply.github.com>
2022-03-15 23:06:29 +1300

    Messages data class closes #39 (#67)
    Adds the Message data class to the database

---
+++ src/main/java/com/team701/buddymatcher/domain/communication/Message.java
@@ -0,0 +1,59 @@
+package com.team701.buddymatcher.domain.communication;
+
+import com.team701.buddymatcher.domain.users.User;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "MESSAGES")
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "SENDER_ID")
+    private User sender;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "RECEIVER_ID")
+    private User receiver;
+    private long timestamp;
+    private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
