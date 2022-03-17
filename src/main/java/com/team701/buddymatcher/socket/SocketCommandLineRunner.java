4b27b021996403da6658150dd467474c67399130
ZHENK <chengzhenyang@gmail.com>
2022-03-18 00:06:15 +1300

    Setup SocketIO to run on spring

---
+++ src/main/java/com/team701/buddymatcher/socket/SocketCommandLineRunner.java
@@ -0,0 +1,22 @@
+package com.team701.buddymatcher.socket;
+
+import com.corundumstudio.socketio.SocketIOServer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SocketCommandLineRunner implements CommandLineRunner {
+
+    private final SocketIOServer server;
+
+    @Autowired
+    public SocketCommandLineRunner(SocketIOServer server) {
+        this.server = server;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        server.start();
+    }
+}
