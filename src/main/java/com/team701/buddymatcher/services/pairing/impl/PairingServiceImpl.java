40de938d585b604bc7259d0aaf0f4bafde2a1c0a
rayaway2020 <69098495+rayaway2020@users.noreply.github.com>
2022-03-17 16:47:38 +1300

    Implement the addBuddy function in pairingService

--- src/main/java/com/team701/buddymatcher/services/pairing/impl/PairingServiceImpl.java
+++ src/main/java/com/team701/buddymatcher/services/pairing/impl/PairingServiceImpl.java
@@ -1,9 +1,16 @@
 package com.team701.buddymatcher.services.pairing.impl;
 
+import com.team701.buddymatcher.domain.users.Buddies;
+import com.team701.buddymatcher.domain.users.User;
 import com.team701.buddymatcher.repositories.users.BuddiesRepository;
+import com.team701.buddymatcher.repositories.users.UserRepository;
 import com.team701.buddymatcher.services.pairing.PairingService;
+import com.team701.buddymatcher.services.users.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 /**
  * Implementation for the PairingService which is a service providing the implementations of the methods
@@ -12,16 +19,39 @@ import org.springframework.stereotype.Service;
 @Service
 public class PairingServiceImpl implements PairingService {
 
+    private final UserRepository userRepository;
     private final BuddiesRepository buddiesRepository;
 
+    private final UserService userService;
+
     @Autowired
-    public PairingServiceImpl(BuddiesRepository buddiesRepository) {
+    public PairingServiceImpl(UserRepository userRepository, BuddiesRepository buddiesRepository, UserService userService) {
+        this.userRepository = userRepository;
         this.buddiesRepository = buddiesRepository;
+        this.userService = userService;
     }
     @Override
+    @Transactional
     public void addBuddy(Long userId, Long buddyId) {
-        //Currently just a blank implementation for testing endpoint call
-        System.out.println(String.format("Buddy add request: %s, %s",userId,buddyId));
+        User user = userService.retrieve(userId);
+        User buddy = userService.retrieve(buddyId);
+
+        Buddies userBuddies = buddiesRepository.findById(user.getBuddies().getId()).orElseThrow();
+        Buddies buddyBuddies = buddiesRepository.findById(buddy.getBuddies().getId()).orElseThrow();
+
+        List<User> userBuddyList =  userBuddies.getUsers();
+        List<User> buddyBuddyList =  buddyBuddies.getUsers();
+        //check for duplication adding buddies
+        if(userBuddyList.contains(buddy) || buddyBuddyList.contains(user)){
+            System.out.println(String.format("Duplication of adding buddies"));
+        }else {
+            userBuddyList.add(buddy);
+            buddyBuddyList.add(user);
+
+            buddiesRepository.save(userBuddies);
+            buddiesRepository.save(buddyBuddies);
+            System.out.println(String.format("Buddy add request: %s, %s",userId,buddyId));
+        }
     }
     
     @Override
