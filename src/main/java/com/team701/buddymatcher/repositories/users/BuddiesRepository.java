77984df87aa1d271b42cd5bee00ef3ec918ac0c7
rayaway2020 <69098495+rayaway2020@users.noreply.github.com>
2022-03-17 16:47:38 +1300

    Implement the addBuddy function in pairingService

--- src/main/java/com/team701/buddymatcher/repositories/users/BuddiesRepository.java
+++ src/main/java/com/team701/buddymatcher/repositories/users/BuddiesRepository.java
@@ -2,8 +2,12 @@ package com.team701.buddymatcher.repositories.users;
 
 import com.team701.buddymatcher.domain.users.Buddies;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 
+@Repository
 public interface BuddiesRepository extends JpaRepository<Buddies, Long>{
 
+    public Optional<Buddies> findById(Long id);
 }
