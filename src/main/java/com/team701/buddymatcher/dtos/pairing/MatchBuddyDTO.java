c87ec9c0149d8dc6135333f82e1a8d96239c6bfb
ff-dev-45 <flynn.fromont@gmail.com>
2022-03-17 15:48:11 +1300

    Added in blank endpoint to PairingService/Controller as well as setup tests for these when logic implemented

---
+++ src/main/java/com/team701/buddymatcher/dtos/pairing/MatchBuddyDTO.java
@@ -0,0 +1,18 @@
+package com.team701.buddymatcher.dtos.pairing;
+
+import java.util.List;
+
+/**
+ * Data Transfer Object representing a request to find a buddy match from a given list of courses
+ */
+public class MatchBuddyDTO {
+    private List<Long> courseIds;
+
+    public List<Long> getCourseIds() {
+        return courseIds;
+    }
+
+    public void setCourseIds(List<Long> courseIds) {
+        this.courseIds = courseIds;
+    }
+}
--- src/main/java/com/team701/buddymatcher/services/pairing/impl/PairingServiceImpl.java
+++ src/main/java/com/team701/buddymatcher/services/pairing/impl/PairingServiceImpl.java
@@ -3,6 +3,8 @@ package com.team701.buddymatcher.services.pairing.impl;
 import com.team701.buddymatcher.services.pairing.PairingService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 /**
  * Implementation for the PairingService which is a service providing the implementations of the methods
  * for the pairing endpoints
@@ -20,5 +22,11 @@ public class PairingServiceImpl implements PairingService {
     public void removeBuddy(Long userId, Long buddyId) {
         //Currently just a blank implementation for testing endpoint call
         System.out.println(String.format("Buddy remove request: %s, %s",userId,buddyId));
+    }
+
+    @Override
+    public void matchBuddy(Long userId, List<Long> courseIds) {
+        //Currently just a blank implementation for testing endpoint call
+        System.out.println(String.format("Buddy match request: %s, %s",userId,courseIds));
     }
 }
