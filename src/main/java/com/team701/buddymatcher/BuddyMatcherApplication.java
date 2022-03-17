98b5ecf35a54972c58a73e0daf250f6df40d4217
JafarMaash <Jmaa831@aucklanduni.ac.nz>
2022-03-17 17:33:54 +1300

    Fix for 401s when trying to access the API
    Disabled security autoconfiguration

--- src/main/java/com/team701/buddymatcher/BuddyMatcherApplication.java
+++ src/main/java/com/team701/buddymatcher/BuddyMatcherApplication.java
@@ -3,11 +3,12 @@ package com.team701.buddymatcher;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class BuddyMatcherApplication {
 
     @Bean
