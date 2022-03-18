74d8e53e87401b7bf2cce90e76c666ad36f83df3
ZHENK <chengzhenyang@gmail.com>
2022-03-18 13:45:24 +1300

    Implement socketio message store and history with online (#97)
    * Implement socketio message store and history with online
    
    * Use exceptions for Long and simplify sendOnline logic

---
+++ src/main/java/com/team701/buddymatcher/socket/UserObject.java
@@ -0,0 +1,14 @@
+package com.team701.buddymatcher.socket;
+
+public class UserObject {
+    private Long userId;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public UserObject setUserId(Long userId) {
+        this.userId = userId;
+        return this;
+    }
+}
