45b1df9190d6b8e4eabe1aca7530dfebf0141c5f
ThomasHolsterUOA <thol600@aucklanduni.ac.nz>
2022-03-18 18:02:43 +1300

    Read ics data from input stream instead of file

--- src/main/java/com/team701/buddymatcher/services/timetable/TimetableService.java
+++ src/main/java/com/team701/buddymatcher/services/timetable/TimetableService.java
@@ -3,13 +3,16 @@ package com.team701.buddymatcher.services.timetable;
 import com.team701.buddymatcher.domain.timetable.Course;
 import com.team701.buddymatcher.domain.timetable.Timetable;
 import com.team701.buddymatcher.domain.users.User;
+import net.fortuna.ical4j.data.ParserException;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 public interface TimetableService {
     Timetable retrieve(String userId);
-    List<String> getCalInfoFromIcs(File file) throws Exception;
+    List<String> getCalInfoFromIcs(InputStream input) throws ParserException, IOException;
     void populateCourses(Long studentId, List<String> courseNames);
 
     List<Course> getCourses(Long userId);
--- src/main/java/com/team701/buddymatcher/services/timetable/impl/TimetableServiceImpl.java
+++ src/main/java/com/team701/buddymatcher/services/timetable/impl/TimetableServiceImpl.java
@@ -7,6 +7,7 @@ import com.team701.buddymatcher.repositories.timetable.CourseRepository;
 import com.team701.buddymatcher.services.timetable.TimetableService;
 import com.team701.buddymatcher.services.users.UserService;
 import net.fortuna.ical4j.data.CalendarBuilder;
+import net.fortuna.ical4j.data.ParserException;
 import net.fortuna.ical4j.model.Calendar;
 import net.fortuna.ical4j.model.Component;
 import net.fortuna.ical4j.model.Property;
@@ -16,12 +17,11 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Timestamp;
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class TimetableServiceImpl implements TimetableService {
@@ -43,18 +43,18 @@ public class TimetableServiceImpl implements TimetableService {
     }
 
     /**
-     * This method parses the input .ics file and retrieves the course names from the file.
+     * This method parses the input .ics stream and retrieves the course names from the file.
      *
-     * @param file file to upload
+     * @param input An input stream containing ics data
      * @return A list of string that contains the courses from the ics file.
      */
     @Override
-    public List<String> getCalInfoFromIcs(File file) throws Exception {
+    public List<String> getCalInfoFromIcs(InputStream input) throws ParserException, IOException {
         Calendar calendar;
-        FileInputStream fin = new FileInputStream(file);
         CalendarBuilder builder = new CalendarBuilder();
-        calendar = builder.build(fin);
-        List<String> result = new ArrayList<>();
+        calendar = builder.build(input);
+
+        Set<String> result = new HashSet<>();
 
         for (Iterator i = calendar.getComponents().iterator(); i.hasNext(); ) {
             Component component = (Component) i.next();
@@ -73,15 +73,8 @@ public class TimetableServiceImpl implements TimetableService {
                 }
             }
         }
-        //Remove the duplicate course names from the calendarInfo list
-        List<String> courseList = new ArrayList<>();
-        for (String course : result) {
-            if (!courseList.contains(course)) {
-                courseList.add(course);
-            }
-        }
 
-        return courseList;
+        return new ArrayList<>(result);
     }
 
     @Transactional
