4b661ac32eec2e9d09bb0a6c298b8d09014e7659
AlexMiao7 <63859123+AlexMiao7@users.noreply.github.com>
2022-04-03 11:12:47 +1200

    add message search and test (#148)

--- src/test/java/com/team701/buddymatcher/controllers/communication/CommunicationControllerIntegrationTest.java
+++ src/test/java/com/team701/buddymatcher/controllers/communication/CommunicationControllerIntegrationTest.java
@@ -62,6 +62,18 @@ public class CommunicationControllerIntegrationTest {
                 .andExpect(jsonPath("$[1].content").value("Yo HARRO"))
                 .andExpect(jsonPath("$[1].timestamp").value("1647394821000"))
                 .andExpect(jsonPath("$[1].read").value(false))
+                .andExpect(jsonPath("$[2].id").value(3))
+                .andExpect(jsonPath("$[2].senderId").value(1))
+                .andExpect(jsonPath("$[2].receiverId").value(2))
+                .andExpect(jsonPath("$[2].content").value("oops"))
+                .andExpect(jsonPath("$[2].timestamp").value("1647397776000"))
+                .andExpect(jsonPath("$[2].read").value(true))
+                .andExpect(jsonPath("$[3].id").value(4))
+                .andExpect(jsonPath("$[3].senderId").value(2))
+                .andExpect(jsonPath("$[3].receiverId").value(1))
+                .andExpect(jsonPath("$[3].timestamp").value("1647398421000"))
+                .andExpect(jsonPath("$[3].content").value("Yo oops"))
+                .andExpect(jsonPath("$[3].read").value(false))
                 .andDo(print());
     }
 
@@ -72,12 +84,33 @@ public class CommunicationControllerIntegrationTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$[0].id").value(2))
                 .andExpect(jsonPath("$[0].name").value("Green Dinosaur"))
-                .andExpect(jsonPath("$[0].lastMessage").value("Yo HARRO"))
-                .andExpect(jsonPath("$[0].timestamp").value("1647394821000"))
+                .andExpect(jsonPath("$[0].lastMessage").value("Yo oops"))
+                .andExpect(jsonPath("$[0].timestamp").value("1647398421000"))
                 .andExpect(jsonPath("$[1].id").value(3))
                 .andExpect(jsonPath("$[1].name").value("Hiruna Smith"))
                 .andExpect(jsonPath("$[1].lastMessage").isEmpty())
                 .andExpect(jsonPath("$[1].timestamp").isEmpty())
+                .andDo(print());
+    }
+
+    @Test
+    void getMessagesByWords() throws Exception {
+        mvc.perform(get("/api/communication/messages/search/{id}",2)
+                        .sessionAttrs(Collections.singletonMap("UserId", 1))
+                        .param("keywords", "Yo"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value(2))
+                .andExpect(jsonPath("$[0].senderId").value(2))
+                .andExpect(jsonPath("$[0].receiverId").value(1))
+                .andExpect(jsonPath("$[0].timestamp").value("1647394821000"))
+                .andExpect(jsonPath("$[0].content").value("Yo HARRO"))
+                .andExpect(jsonPath("$[0].read").value(false))
+                .andExpect(jsonPath("$[1].id").value(4))
+                .andExpect(jsonPath("$[1].senderId").value(2))
+                .andExpect(jsonPath("$[1].receiverId").value(1))
+                .andExpect(jsonPath("$[1].timestamp").value("1647398421000"))
+                .andExpect(jsonPath("$[1].content").value("Yo oops"))
+                .andExpect(jsonPath("$[1].read").value(false))
                 .andDo(print());
     }
 }
